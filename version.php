b973587b987958f4b9f51e5e76966a75b2527079
Doug Cooper <douglas.cooper@qmul.ac.uk>
2016-02-09 09:49:29 +0000

    Remove unsupported $module syntax from version.php

--- version.php
+++ version.php
@@ -4,6 +4,10 @@
  * @copyright 2012 Turnitin
  */
 
+/* $module syntax unsupported in Moodle 3.0.
+ * Use $plugin instead.
+ * D Cooper 09/02/2016
+ *
 if(!isset($module)){
 	$module = new stdClass();
 }
@@ -13,12 +17,15 @@ $module->maturity  = MATURITY_STABLE;
 $module->cron     = 1800;        // Period for cron to check this module (secs)
 //$module->requires = 2007101509;  // 1.9+
 //$module->requires = 2010112400;  // 2.0+
+ *
+ * End unsupported syntax.
+ */
 
 if (!isset($plugin)) {
 	$plugin = new StdClass();
 }
 
-$plugin->version  = 2015030305;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016020900;  // The current module version (Date: YYYYMMDDXX)
 $plugin->component = 'mod_turnitintool';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 1800;        // Period for cron to check this module (secs)
